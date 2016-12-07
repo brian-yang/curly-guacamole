31b02f3b4bf7b377bbb74b206536f8dbca43cf41
Brian Yang <brian.yang328@gmail.com>
2016-12-06 22:34:09 -0500

    Fix authenticate routing

--- app.py
+++ app.py
@@ -10,13 +10,36 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/authenticate/')
-def auth():
-    return render_template('authenticate.html')
+@app.route("/authenticate/", methods = ["GET", "POST"])
+def authenticate():
+    if 'submit' not in request.form:
+        return render_template("authenticate.html")
+
+    if request.form['submit'] == 'register':
+        if not request.form['username'] or not request.form['password']:
+            msg = "Please enter a username and password."
+        elif auth.register(request.form['username'], request.form['password']):
+            msg = "Successfully registered!"
+        else:
+            msg = "Failed to register! Username is taken."
+        return render_template("authenticate.html", message = msg)
+
+    elif request.form['submit'] == 'login':
+        if not request.form['username'] or not request.form['password']:
+            msg = "Please enter a username and password."
+        elif auth.login(request.form['username'], request.form['password']):
+            session["user"] = request.form['username']
+            return redirect(url_for("profile"))
+        else:
+            msg = "Failed to login. Username and/or password incorrect."
+        return render_template("authenticate.html", message = msg)
 
 @app.route('/profile/')
 def profile():
-    return render_template('profile.html')
+    if 'user' in session:
+        return render_template('profile.html')
+    else:
+        return redirect(url_for("home"))
 
 @app.route('/display/')
 def display():
