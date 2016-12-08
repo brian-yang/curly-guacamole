ec8de1d381223e7986c7172a19d7069975f52392
jieyuxu <jieyu.stuy@gmail.com>
2016-12-07 22:31:50 -0500

    got nutritionix api's data to display and search bar works

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, session, request, redirect, url_for
 from utils import auth
 from nutritionix import Nutritionix
+import json
+
 nix = Nutritionix(app_id="b31ef4ce", api_key="070678e0943ef0af60625a44d7de3bb3")
 app = Flask(__name__)
 
@@ -44,13 +46,23 @@ def profile():
     else:
         return redirect(url_for("home"))    
 
-@app.route('/display/')
+@app.route('/display/', methods = ["GET", "POST"])
 def display():
-    return render_template('display.html')
+    if 'search' in request.form.keys():
+        if not request.form['lookup']:
+            return redirect(url_for("home"))
+        else:
+            call = nix.search(request.form['lookup'], results="0:1").json()
+            call = call['hits'][0]["_id"]
+            call = nix.item(id=call).json()
+            return render_template('display.html', fooddata = call, foodname=request.form['lookup'])
+    else:
+        return redirect(url_for("home"))
 
 # ===========================================
 # RUN
 # ===========================================
+
 if __name__ == '__main__':
     app.debug = True
     app.config.from_object('config')
